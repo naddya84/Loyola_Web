2e2080151d09d6a5ef7b61a4262be9e0707a49aa
DkChriss <chrisshuayhua@hotmail.com>
2021-12-02 14:23:47 -0400

    Fix name vars for add database local

--- services/get_credit_extract.php
+++ services/get_credit_extract.php
@@ -5,12 +5,12 @@ require_once '../config/configure.php';
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 
-$docuCage = $_GET['docu-cage'];
-$credNumber = $_GET['cred-number'];
+$docuCage = isset($_GET['docu_cage'])?$_GET['docu_cage']:null; //associate code
+$credNumber = isset($_GET['cred_number'])?$_GET['cred_number']:null; //credit number
 
-if(!empty($docuCage)) {
+if(!is_null($docuCage) && !is_null($credNumber)) {
 
-    $data = ORM::for_table('credit_extract')
+    $creditsExtract = ORM::for_table('credit_extract')
         ->select('credit_extract.*')
         ->join('user', ['credit_extract.user_id','=','user.id'])
         ->join('credit_history', ['credit_extract.id_credit','=','credit_history.id'])
@@ -18,35 +18,55 @@ if(!empty($docuCage)) {
         ->where('user.id_member', $docuCage)
         ->find_array();
         
-    if(!is_null($data) && !empty($data)) {
+    if(!is_null($creditsExtract) && !empty($creditsExtract)) {
 
-        $dataDetail = ORM::for_table('credit_extract_detail')
-        ->where("id_credit_extract", $data[0]["id"])
+        $creditsExtractDetail = ORM::for_table('credit_extract_detail')
+        ->where("id_credit_extract", $creditsExtract[0]["id"])
         ->order_by_asc('credit_extract_detail.credNroTrans')
         ->find_array();
         
-        $dataArray = array(
-            "member" => $docuCage,
-            "error" => false,
-            "result" => $data,
-            "detail" => $dataDetail
-        );
+        if(!empty($creditsExtractDetail)) {
 
-        echo json_encode($dataArray);
+            echo json_encode([
+                'error'=> false,
+                'errorMessage'=> [],
+                'errorCode' => 0,
+                'result' => $creditsExtract,
+                'detail' => $creditsExtractDetail
+            ]);
+
+        } else {
+            echo json_encode([
+                'error'=> false,
+                'errorMessage'=> [],
+                'errorCode' => 0,
+                'result' => $creditsExtract,
+                'detail' => []
+            ]);
+        }
 
     } else {
         
-        $data = array("error" => true, "msg" => 'No se logro encontrar el extracto de credito');
-
-        echo json_encode($data);
+        echo json_encode([
+            'error'=> true,
+            'errorMessage'=> [
+                "1" =>'No se logro encontrar algun extracto de credito registrado'
+            ],
+            'errorCode' => 0,
+            'result' => []
+        ]);
     }
-
 
 } else {
     
-    $data = array("error" => true, "msg" => 'ID usuario no definido');
-
-    echo json_encode($data);
+    die(json_encode([
+        'error'=> true,
+        'errorMessage'=> [
+            "1" =>'Valor docu-cage no especificado',
+            "2" =>'Valor cred-number no especificado'
+        ],
+        'errorCode' => 0,
+        'result' => []
+    ]));
 }
-
 ?>
